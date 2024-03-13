63b3ae7b0c2443ea2ebf176e8c5e91db09e3a2bd
viniciosragazzidev <devviniciosragazzi@gmail.com>
2024-03-13 17:29:19 -0300

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ActionLogin from "./login/loginForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">Hello World</h1>
+      <h1 className="text-3xl font-bold">Hello World men</h1>
     </main>
   );
 }
